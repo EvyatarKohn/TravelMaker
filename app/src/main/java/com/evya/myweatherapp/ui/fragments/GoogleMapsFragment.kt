51f101052f10630b0de2393eafb63f260d03d014
Evyatar Cohen <evyatar.kohn@gmail.com>
2021-09-28 14:51:45 +0300

    add auto complete to google mpas serch

--- app/src/main/java/com/evya/myweatherapp/ui/fragments/GoogleMapsFragment.kt
+++ app/src/main/java/com/evya/myweatherapp/ui/fragments/GoogleMapsFragment.kt
@@ -3,24 +3,29 @@ package com.evya.myweatherapp.ui.fragments
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.evya.myweatherapp.R
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.google_maps_fragment_layout.*
+
 
 @AndroidEntryPoint
 class GoogleMapsFragment : Fragment() {
@@ -29,17 +34,34 @@ class GoogleMapsFragment : Fragment() {
     private lateinit var mGoogleMap: GoogleMap
     private lateinit var mLat: String
     private lateinit var mLong: String
-//    private lateinit var mPlaceAutoCompleteAdapter: PlaceAutocompleteAdapter
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mNavController = Navigation.findNavController(view)
+        if (!Places.isInitialized()) {
+            activity?.applicationContext?.let {
+                Places.initialize(
+                    it,
+                    getString(R.string.google_maps_key)
+                )
+                Places.createClient(it)
+            }
+        }
 
-        search_bar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(p0: String?): Boolean {
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i("GoogleMapsFragment", "Place: ${place.name}, ${place.id}")
                 mGoogleMap.clear()
-                val location = search_bar.query.toString()
+                val location = place.toString()
                 val geocoder = Geocoder(activity?.applicationContext)
                 val list = geocoder.getFromLocationName(location, 1) as ArrayList<Address>
                 if (list.size > 0) {
@@ -48,18 +70,19 @@ class GoogleMapsFragment : Fragment() {
                     mLong = address.longitude.toString()
                     val latLang = LatLng(address.latitude, address.longitude)
                     mGoogleMap.addMarker(MarkerOptions().position(latLang))
-                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLang, 10f))
+                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLang, 18f))
                 }
-                return false
             }
 
-            override fun onQueryTextChange(p0: String?): Boolean {
-                return false
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i("GoogleMapsFragment", "An error occurred: $status")
             }
         })
 
         show_weather_btn.setOnClickListener {
-            val bundle = bundleOf("lat" to mLat.toFloat(), "long" to mLong.toFloat(), "fromMaps" to true)
+            val bundle =
+                bundleOf("lat" to mLat.toFloat(), "long" to mLong.toFloat(), "fromMaps" to true)
             mNavController.navigate(R.id.action_googleMapsFragment_to_cityFragment, bundle)
         }
     }
@@ -73,7 +96,7 @@ class GoogleMapsFragment : Fragment() {
 
 
         val mapView: SupportMapFragment =
-            (childFragmentManager.findFragmentById(R.id.map_layout)) as SupportMapFragment
+            (childFragmentManager.findFragmentById(R.id.map_layout) as SupportMapFragment)
 
         mLat = arguments?.get("lat").toString()
         mLong = arguments?.get("long").toString()
@@ -89,7 +112,6 @@ class GoogleMapsFragment : Fragment() {
             mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
             mGoogleMap.setOnMapLoadedCallback {
                 show_weather_btn.visibility = View.VISIBLE
-                search_bar.visibility = View.VISIBLE
             }
             mGoogleMap.setOnMapClickListener { latLng ->
                 mGoogleMap.clear()
