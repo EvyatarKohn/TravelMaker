3c308461cd9493f6fa2d946d5a45fc00d839b698
Evyatar Cohen <evyatar.kohn@gmail.com>
2021-11-15 12:19:30 +0200

    Remove logs from master branch

--- app/src/main/java/com/evya/myweatherapp/di/AppModule.kt
+++ app/src/main/java/com/evya/myweatherapp/di/AppModule.kt
@@ -1,15 +1,13 @@
 package com.evya.myweatherapp.di
 
 import com.evya.myweatherapp.network.TripApi
+import com.evya.myweatherapp.network.WeatherApi
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.evya.myweatherapp.network.WeatherApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,9 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    private var logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    private val client =OkHttpClient.Builder().addInterceptor(logging).build()
 
     @Singleton
     @Provides
@@ -35,7 +30,6 @@ object AppModule {
         Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/")
             .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(client)
             .build()
             .create(WeatherApi::class.java)
 
@@ -45,7 +39,6 @@ object AppModule {
         Retrofit.Builder()
             .baseUrl("https://api.opentripmap.com/0.1/en/")
             .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(client)
             .build()
             .create(TripApi::class.java)
 
