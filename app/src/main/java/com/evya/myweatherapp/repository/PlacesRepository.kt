1eec073ffe668066d3388283fd70c499c66ba782
YDT_DOM\evyatar-c <evyatar-c@yedioth.co.il>
2023-06-26 08:41:19 +0300

    Add "key" to constants

--- app/src/main/java/com/evya/myweatherapp/repository/PlacesRepository.kt
+++ app/src/main/java/com/evya/myweatherapp/repository/PlacesRepository.kt
@@ -1,5 +1,6 @@
 package com.evya.myweatherapp.repository
 
+import com.evya.myweatherapp.Constants.PLACES_REPOSITORY_API
 import com.evya.myweatherapp.MainData.attractionRadius
 import com.evya.myweatherapp.network.TripApi
 import javax.inject.Inject
@@ -13,7 +14,7 @@ class PlacesRepository @Inject constructor(private val tripApi: TripApi) {
             lat,
             kind,
 //            "1000",// Default is 500
-            "5ae2e3f221c38a28845f05b64bacfee3164cdfa9999c5226a8a0508f"
+            PLACES_REPOSITORY_API
         )
 
 }
